a5e46409bdd225c650dd086be49222a5d73288f7
Hopi Bee <hopibee@gmail.com>
2021-12-31 15:53:14 +1100

    updated the index.php file to fix the emergency

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
 <h1>Hello world!</h1>
 <div><img src="img_hello_world.jpg" alt="Hello world from space" style="width:100%;max-width:960px"></div>
 <p>This is the first file in my new Git Repo.</p>
-<p>a new line of text!</p>
+<p>We fixed the emergency here!</p>
+
 
 </body>
 </html>
