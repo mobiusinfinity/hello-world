6725ae2472818dfdf12cff283f0c53dabd2196f0
Hopi Bee <hopibee@gmail.com>
2021-12-31 19:27:24 +1100

    updated index.php. Resized image and added a line of text.

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
 
 <h1>Hello world!</h1>
 
-<div><img src="img_hello_world.jpg" alt="Hello world from space" style="width:100%;max-width:960px"></div>
+<div><img src="img_hello_world.jpg" alt="Hello world from space" style="width:100%;max-width:640px"></div>
 
 <p>This is the first file in my new Git Repo.</p>
 <p>This is to show how merging works!</p>
-
+<p>This is another line to be pushed from local to remote.</p>
 <div><img src="hello-world-2.jpg" alt="Hello Git" style="width:100%;max-width:640px"></div>
 
 </body>
