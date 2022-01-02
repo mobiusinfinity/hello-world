616960283c537aba24af1c2d06e37c541b428a30
Hopi Bee <hopibee@gmail.com>
2022-01-02 12:36:52 +1100

    from laptop, added image of cat in space

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <h1>Hello world!</h1>
 
 <div><img src="img_hello_world.jpg" alt="Hello world from space" style="width:100%;max-width:640px"></div>
-
+<div><img src="catspace.jfif" alt="Cat in Space" style="width:100%;max-width:640px"></div>
 <p>This is the first file in my new Git Repo.</p>
 <p>This is to show how merging works!</p>
 <p>This is another line to be pushed from local to remote.</p>
